1cb244c5309e7b37d6b1ccc38ed94242c2160d75
felipe.odoni <felipe.odoni@gamesys.co.uk>
2019-02-22 16:47:09 +0000

    Initial commit

---
+++ src/main/java/com/odoni/nfacts/NfactsApplication.java
@@ -0,0 +1,13 @@
+package com.odoni.nfacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NfactsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/odoni/nfacts/NfactsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.odoni.nfacts;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NfactsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
